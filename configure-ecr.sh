4b9c6f938629fdfb6af953b1c8d64f7cf2ad1d64
Kerin Cosford <cosford@gmail.com>
2017-04-03 23:55:53 +0100

    playing with codeship

--- configure-ecr.sh
+++ configure-ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws configure set default.region eu-west-1
+aws configure set default.region ${AWS_REGION}
 
 if ! aws ecr describe-repositories --repository-names ${CI_REPO_NAME} > /dev/null 2>&1;
     then aws ecr create-repository --repository-name ${CI_REPO_NAME};
