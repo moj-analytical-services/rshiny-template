95a2ecc1d64171734b461dc87bebabfd69db8e00
Kerin Cosford <kerin.cosford@digital.justice.gov.uk>
2017-04-04 12:24:23 +0100

    Cleaned up failed attempts to use external hosted CI/CD

--- configure-ecr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws configure set default.region ${AWS_REGION}
-
-if ! aws ecr describe-repositories --repository-names ${CI_REPO_NAME} > /dev/null 2>&1;
-    then aws ecr create-repository --repository-name ${CI_REPO_NAME};
-fi
